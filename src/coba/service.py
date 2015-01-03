89cfed187956e810717ece1bd4317b8222e8f60b
Florian Brucker <mail@florianbrucker.de>
2015-01-03 13:39:07 +0100

    Switched to dual service architecture.

--- src/coba/service.py
+++ src/coba/service.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
-"""
-The coba background service.
-"""
 
+import errno
 import logging
 import logging.handlers
 import os
 import time
 
-import daemon
-import lockfile
+from daemon import DaemonContext
+import lockfile.pidlockfile
+
 
 _NAME = 'coba-daemon'
-_PID_FILE_PATH = '/home/torf/tmp/%s.pid' % _NAME
+
 
 logger = logging.getLogger(_NAME)
 _handler = logging.handlers.SysLogHandler(address='/dev/log')
@@ -22,9 +21,20 @@ _handler.setFormatter(logging.Formatter(_format))
 logger.addHandler(_handler)
 logger.setLevel(logging.DEBUG)
 
+def log(msg):
+    logger.info('(%d) %s' % (os.getpid(), msg))
 
-# Taken from python-daemon
+
 def detach_process():
+    """
+    Detach daemon process.
+
+    Forks the current process into a parent and a detached child. The
+    child process resides in its own process group, has no controlling
+    terminal attached and is cleaned up by the init process.
+
+    Returns ``True`` for the parent and ``False`` for the child.
+    """
     # To detach from our process group we need to call ``setsid``. We
     # can only do that if we aren't a process group leader. Therefore
     # we fork once, which makes sure that the new child process is not
@@ -32,7 +42,7 @@ def detach_process():
     pid = os.fork()
     if pid > 0:
         # Parent process
-        return pid
+        return True
     os.setsid()
     # We now fork a second time and let the second's fork parent exit.
     # This makes the second fork's child process an orphan. Orphans are
@@ -41,56 +51,87 @@ def detach_process():
     # leader and can therefore never acquire a controlling terminal.
     pid = os.fork()
     if pid > 0:
-        logger.info("Second fork's parent here, I'm exiting.")
+        log("Second fork's parent here, I'm exiting.")
         os._exit(os.EX_OK)
-    return 0
+    return False
+
+
+class PIDFile(lockfile.pidlockfile.PIDLockFile):
+
+    def __init__(self, *args, **kwargs):
+        lockfile.pidlockfile.PIDLockFile.__init__(self, *args, **kwargs)
+        self.pid = None
+
+    def acquire(self, *args, **kwargs):
+        self.pid = os.getpid()
+        return lockfile.pidlockfile.PIDLockFile.acquire(self, *args, **kwargs)
+
 
 
 class Daemon(object):
 
-    def __init__(self):
-        self._pid_file = lockfile.FileLock(_PID_FILE_PATH)
-        self._context = daemon.DaemonContext(
-            detach_process=False,  # We're doing this manually
-        )
+    def _start(self, pid_file_path):
+        log('Performing double-fork.')
 
-    def start(self):
-        logger.info("Parent process ID: %d" % os.getpid())
-        logger.info("Parent process group: %d" % os.getpgrp())
-        logger.info('Starting daemon.')
+        if detach_process():
+            log("First fork's parent here, I'm returning.")
+            return
+
+        log("Second fork's child here, I'm continuing.")
+        pid_file = PIDFile(pid_file_path)
+
         try:
-            self._pid_file.acquire(timeout=0)
-            with open(_PID_FILE_PATH, 'w') as f:
-                f.write('%d' % self._pid_file.pid)
-            logger.info('Lock file acquired.')
-
-            logger.info('Performing double-fork.')
-            if detach_process() > 0:
-                logger.info("First fork's parent here, I'm returning.")
-                return
-            logger.info("Second fork's child here, I'm continuing.")
-
-            with self._context:
+            pid_file.acquire(timeout=0)
+            log('Lock file acquired.')
+            with DaemonContext(detach_process=False):
+                log('Executing daemon code.')
                 self.run()
         except Exception as e:
             logger.exception(e)
-            raise
         finally:
             try:
-                self._pid_file.release()
-                logger.info('Lock file released.')
-            except (lockfile.NotLocked, lockfile.NotMyLock):
-                pass
-        logger.info('Daemon is exiting.')
+                pid_file.release()
+                log('Lock file released.')
+            except Exception as e:
+                logger.exception(e)
+
+        # We need to shutdown the daemon process at this point, because
+        # otherwise it will continue executing from after the original
+        # call to ``DaemonController.start``.
+        log('Daemon is exiting.')
+        os._exit(os.EX_OK)
 
     def run(self):
-        logger.info("I'm running!")
-        logger.info("Daemon process ID: %d" % os.getpid())
-        logger.info("Daemon process group: %d" % os.getpgrp())
+        log('This is the run method.')
+        time.sleep(5)
+
+
+class DaemonController(object):
+
+    def __init__(self, daemon, pid_file_path):
+        self._daemon = daemon
+        self._pid_file = PIDFile(pid_file_path)
+
+    def start(self):
+        logger.info('******************************************')
+        pid = self.get_pid()
+        if pid:
+            log('ERROR: Daemon is already running at PID %d.' % pid)
+            raise ValueError('Daemon is already running at PID %d.' % pid)
+        log('Starting daemon.')
+        self._daemon._start(self._pid_file.path)
+
+    def is_running(self):
+        return self.get_pid() is not None
+
+    def call(self, fun, *args):
+        if not self.is_running():
+            raise ValueError('Daemon is not running.')
+        raise NotImplementedError()
 
     def get_pid(self):
         """
         Get PID of daemon process or ``None`` if daemon is not running.
         """
-        with open(_PID_FILE_PATH) as f:
-            return int(f.read())
+        return self._pid_file.read_pid()
+
